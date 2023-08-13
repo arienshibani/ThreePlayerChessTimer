24e6bda07127417f689463a0c688b5c318941033
Arien Shibani <aaari94@gmail.com>
2023-08-13 18:32:55 +0200

    feat(Added better styling and fixed a few bugs in the clock):

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,74 @@
 <script>
   import { onMount } from "svelte";
-  let currentPlayer = 0; // Player 0, 1, 2
+	import { tweened } from 'svelte/motion';
+
+
+  let currentPlayer = 0;
   const playerColors = ["WHITE", "BLACK", "RED"];
+  let timeRemaining = [600, 600, 600];
+  let timers = [null, null, null]; // Individual timers for each player
+  let gameOver = false;
 
 
-  // The default time remaining for each player is 10 minutes.
-  let timeRemaining = [600, 600, 600]; 
-
-  // Parse URL parameters and update timeRemaining, should they wish to have shorter time.
+  /**
+   * @name updateTimersFromURL
+   * @description Parses URL parameters to set custom timers if desired.
+   * 
+   *  - E.g: http://localhost/?time=300 (sets all players to 5 minutes)
+   *  - E.g #2: http://localhost/?p1=60&p2=120&p3=300 (player one and two have 1 and 2 minutes respectively, player 3 has 5 minutes)
+   * 
+  */
   const updateTimersFromURL = () => {
     const searchParams = new URLSearchParams(window.location.search);
-    if(searchParams.has('time')){
-      //set all timers to the same time
-      timeRemaining = [parseInt(searchParams.get('time')), parseInt(searchParams.get('time')), parseInt(searchParams.get('time'))];
+    if (searchParams.has("time")) {
+      //Set all timers to the same time
+      timeRemaining = [
+        parseInt(searchParams.get("time")),
+        parseInt(searchParams.get("time")),
+        parseInt(searchParams.get("time")),
+      ];
     }
     // Set individual players times.
-    if (searchParams.has('p1')) {
-      timeRemaining[0] = parseInt(searchParams.get('p1'));
+    if (searchParams.has("p1")) {
+      timeRemaining[0] = parseInt(searchParams.get("p1"));
     }
-    if (searchParams.has('p2')) {
-      timeRemaining[1] = parseInt(searchParams.get('p2'));
+    if (searchParams.has("p2")) {
+      timeRemaining[1] = parseInt(searchParams.get("p2"));
     }
-    if (searchParams.has('p3')) {
-      timeRemaining[2] = parseInt(searchParams.get('p3'));
+    if (searchParams.has("p3")) {
+      timeRemaining[2] = parseInt(searchParams.get("p3"));
     }
   };
 
-  onMount(() => {
-    window.addEventListener('keydown', handleKeyDown);
-    updateTimersFromURL();
-
-    onDestroy(() => {
-      window.removeEventListener('keydown', handleKeyDown);
-    });
-  });
-
-  let timers = [null, null, null]; // Individual timers for each player
-
+  // Start counting down the current player's time.
   const startTurn = () => {
     timers[currentPlayer] = setInterval(() => {
-      timeRemaining[currentPlayer] -= 1;
+      // Check if current player has time left.
+      if (timeRemaining[currentPlayer] >= 0) {
+        // Check if any other players have time left.
+        const otherPlayers = timeRemaining.filter(
+          (time, index) => index !== currentPlayer
+        );
 
-      // If time is up, clear the timer and remove him from the game.
+        // If there is no other player with time left, the current player wins.
+        if (otherPlayers.every((time) => time <= 0)) {
+          clearInterval(timers[currentPlayer]);
+          gameOver = true;
+          return;
+        }
+        // If there is, decrease it by one.
+        timeRemaining[currentPlayer] -= 1;
+      }
+
+      // If time is up, clear that players time.
       if (timeRemaining[currentPlayer] <= 0) {
         clearInterval(timers[currentPlayer]);
-
-        nextPlayer();
+        // If any players have time left, continue with the next players timer.
+        if (timeRemaining.some((time) => time > 0)) {
+          nextPlayer();
+        }
       }
+
     }, 1000);
   };
 
@@ -56,100 +78,153 @@
     startTurn();
   };
 
-  startTurn();
-
   // Global keydown event handler
   const handleKeyDown = (event) => {
+    if(gameOver){
+      return;
+    }
     if (event.key === " ") {
+      nextPlayer();
+    }
+
+    if (event.key === "Enter") {
+      // Pause the timer
+      startTurn();
+    }
+  };
+
+
+  const handleTouch = (event) => {
+    if(gameOver){
+      return;
+    }
+    
+    if(event.touches.length === 2){
+      nextPlayer();
+    }
+  };
+
+  const handleClicks = (event) => {
+    if(gameOver){
+      return;
+    }
+
+    if(event.detail === 1){
       nextPlayer();
     }
   };
 
   onMount(() => {
+    updateTimersFromURL();
     // Attach the event listener
     window.addEventListener("keydown", handleKeyDown);
-
-    // Cleanup when the component is destroyed
-    onDestroy(() => {
-      window.removeEventListener("keydown", handleKeyDown);
-    });
+    window.addEventListener("touchstart", handleTouch);
+    window.addEventListener("click", handleClicks);
   });
 </script>
 
 <main>
-  <div class="chess-clock"
->
+  <div class="chess-clock">
+
+    {#if gameOver}
+    <div class="gameOver">
+      <h2>{playerColors[currentPlayer]} wins! 👑</h2>
+    </div>
+    {/if}
+
     {#each [0, 1, 2] as playerIndex}
-      <button
-        style={`background-color: ${
-          timeRemaining[playerIndex] <= 0 ? "red" : "white"
-        }`}
+      <div
+        style={`background-image: ${
+          timeRemaining[playerIndex] <= 0 ? "" : ""
+        } `}
         class={playerIndex === currentPlayer ? "active" : ""}
       >
         <div class="playerInfo" id={playerColors[playerIndex]}>
-          <div class="playerColor">
+          <div>
             {playerColors[playerIndex]}
           </div>
 
           <div class="timeLeft">
-            {Math.floor(timeRemaining[playerIndex] / 60)}:{(
-              timeRemaining[playerIndex] % 60
-            )
-              .toString()
-              .padStart(2, "0")}
+            {
+              timeRemaining[playerIndex] <= 0
+                ? "Time's up! ☠️"
+                : `${Math.floor(timeRemaining[playerIndex] / 60)}:${(timeRemaining[playerIndex] % 60).toString().padStart(2, "0")}`
+            }
           </div>
         </div>
-      </button>
+      </div>      
     {/each}
+
+
   </div>
+
+
 </main>
 
 <style>
-
-
-  main {
-    position: absolute;
-    height: 100%;
-    width: 100%;
+  h2 {
+    font-size: 5vh;
+    text-align: center;
   }
 
+  main {
+    font-family: sans-serif;
+    display: flow;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    transition: 0.5s ease-in-out;
+
+  }
+
+  .active {
+    background-image: linear-gradient(83deg, rgba(34,193,195,0) 0%, rgba(10,100,500) 100%) !important;
+  }
 
   .playerInfo{
-    margin-top: 40vh;
+    user-select: none;
+    cursor: pointer; 
+    padding: 5rem;
+    margin: auto;
+    width: 100%;
     height: 100%;
+    font-size: 3vh;
+    font-weight: bold;
+  }
+
+  .gameOver{
+    user-select: none;
+  }
+
+  .timeLeft{
+    font-size: 5vh;
+  }
+
+  @media only screen and (min-width: 800px) {
+    main {
+      padding: 0rem;
+      display: flow;
+    }
   }
 
   .chess-clock {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    width: 100vw;
     overflow: hidden;
     font-size: 2rem;
   }
 
   .chess-clock > button {
-    /* padding: 1rem; */
     border: 1px solid #ccc;
-    /* border-radius: 0.5rem; */
-    margin: 0 0;
     cursor: pointer;
-    height: 100%;
-    width: 100%;
     text-align: center;
-    font-size: 200%;
-    /* align-items: center; */
-    /* margin: 50%, 50%; */
-    /* transform: translate(-50%, -50%); */
   }
-
 
   #WHITE {
-    color: black;
+    color: slategrey;
   }
+
   #BLACK {
-    color: slategray;
+    color: black;
   }
   #RED {
     color: crimson;
